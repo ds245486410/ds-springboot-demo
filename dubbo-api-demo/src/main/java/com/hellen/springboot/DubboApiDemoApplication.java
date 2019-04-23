077e6af6ab5f496693baac0379b3cd83dad2caff
daiweihang <daiweihang-dx@bestpay.com.cn>
2019-04-23 19:09:33 +0800

    增加dubbo

---
+++ dubbo-api-demo/src/main/java/com/hellen/springboot/DubboApiDemoApplication.java
@@ -0,0 +1,13 @@
+package com.hellen.springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DubboApiDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DubboApiDemoApplication.class, args);
+	}
+
+}
