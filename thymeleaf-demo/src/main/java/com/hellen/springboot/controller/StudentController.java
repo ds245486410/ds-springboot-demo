de9b24d6cb667bd2a0cea24a7f3f50cda9894c78
daiweihang <daiweihang-dx@bestpay.com.cn>
2019-04-26 11:43:22 +0800

    增加thymeleaf

---
+++ thymeleaf-demo/src/main/java/com/hellen/springboot/controller/StudentController.java
@@ -0,0 +1,38 @@
+package com.hellen.springboot.controller;
+
+import com.hellen.springboot.dto.StudentDTO;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * @author daiweihang
+ * @date 2019/4/26
+ */
+@Controller
+@RequestMapping("/student")
+public class StudentController {
+
+    @RequestMapping("/index")
+    public String index(Model model){
+        List<StudentDTO> list = new ArrayList<>();
+        StudentDTO studentDTO = null;
+        studentDTO = new StudentDTO();
+        studentDTO.setId(1L);
+        studentDTO.setName("张三");
+        studentDTO.setAge("10");
+        list.add(studentDTO);
+        studentDTO = new StudentDTO();
+        studentDTO.setId(2L);
+        studentDTO.setName("李四");
+        studentDTO.setAge("11");
+        list.add(studentDTO);
+        model.addAttribute("list",list);
+        model.addAttribute("student" , studentDTO);
+        return "index";
+    }
+
+}
