b6aa9e5c1962186e014b92ae419b65d4cfff831f
daiweihang <daiweihang-dx@bestpay.com.cn>
2019-05-07 16:26:36 +0800

    增加thymeleaf信息

--- dubbo-api-demo/src/main/java/com/hellen/springboot/dto/StudentDTO.java
+++ dubbo-api-demo/src/main/java/com/hellen/springboot/dto/StudentDTO.java
@@ -3,6 +3,7 @@ package com.hellen.springboot.dto;
 import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
+import org.springframework.format.annotation.DateTimeFormat;
 
 import java.io.Serializable;
 import java.util.Date;
@@ -16,6 +17,11 @@ public class StudentDTO implements Serializable {
     private Long id;
     private String name;
     private String age;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
     private Date date;
     private Boolean passed;
+
+    public String getPassedDesc(){
+        return passed ? "是"  : "否";
+    }
 }
--- thymeleaf-demo/src/main/java/com/hellen/springboot/controller/StudentController.java
+++ thymeleaf-demo/src/main/java/com/hellen/springboot/controller/StudentController.java
@@ -1,11 +1,14 @@
 package com.hellen.springboot.controller;
 
 import com.hellen.springboot.dto.StudentDTO;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 import java.util.List;
 
 /**
@@ -13,6 +16,7 @@ import java.util.List;
  * @date 2019/4/26
  */
 @Controller
+@Slf4j
 @RequestMapping("/student")
 public class StudentController {
 
@@ -26,6 +30,7 @@ public class StudentController {
         studentDTO.setName("张三");
         studentDTO.setAge("10");
         studentDTO.setPassed(false);
+        studentDTO.setDate(new Date());
         list.add(studentDTO);
 
         studentDTO = new StudentDTO();
@@ -33,6 +38,7 @@ public class StudentController {
         studentDTO.setName("李四");
         studentDTO.setAge("11");
         studentDTO.setPassed(true);
+        studentDTO.setDate(new Date());
         list.add(studentDTO);
 
         studentDTO = new StudentDTO();
@@ -40,10 +46,17 @@ public class StudentController {
         studentDTO.setName("王五");
         studentDTO.setAge("12");
         studentDTO.setPassed(true);
+        studentDTO.setDate(new Date());
         list.add(studentDTO);
 
         model.addAttribute("list",list);
-
+        HashMap<String, String> headers = new HashMap<>();
+        headers.put("id" , "id");
+        headers.put("name" , "姓名");
+        headers.put("age" , "年龄");
+        headers.put("date" , "日期");
+        headers.put("passed" , "结果");
+        model.addAttribute("heaers" , headers);
         model.addAttribute("student" , studentDTO);
         model.addAttribute("datePlanted" , "准高三");
         return "index";
@@ -52,6 +65,39 @@ public class StudentController {
     @RequestMapping("/show")
     public String show(Model model){
         return "show";
+    }
+
+    @RequestMapping("/add")
+    public String add(StudentDTO studentDTO , Model model){
+        log.info("添加 ： [{}]" , studentDTO);
+        model.addAttribute("student" ,  studentDTO);
+        return "showDetail";
+    }
+    @RequestMapping("/addPanel")
+    public String addPanel(Model model){
+        model.addAttribute("actionUrl" , "/student/add");
+        return "add";
+    }
+
+
+    @RequestMapping("/update")
+    public String update(StudentDTO studentDTO , Model model){
+        log.info("更新信息 ： [{}]",studentDTO);
+        model.addAttribute("student" , studentDTO);
+        return "showDetail";
+    }
+
+    @RequestMapping("/delete")
+    public String delete(Long id , Model model){
+        log.info("删除的id  : [{}] " , id);
+        StudentDTO studentDTO = new StudentDTO();
+        studentDTO.setId(1L);
+        studentDTO.setAge("11");
+        studentDTO.setPassed(false);
+        studentDTO.setName("张三");
+        studentDTO.setDate(new Date());
+        model.addAttribute("student" , studentDTO);
+        return "showDetail";
     }
 
 }
