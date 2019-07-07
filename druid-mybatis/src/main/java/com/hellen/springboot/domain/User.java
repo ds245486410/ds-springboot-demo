1b35d82bc659f098f1d926034832b26bb2d979df
hellen <1023483215@qq.com>
2019-07-07 14:15:58 +0800

    增加dubbo的demo

--- druid-mybatis/src/main/java/com/hellen/springboot/domain/User.java
+++ druid-mybatis/src/main/java/com/hellen/springboot/domain/User.java
@@ -4,27 +4,28 @@ import lombok.Getter;
 import lombok.Setter;
 import lombok.ToString;
 
+import java.util.Date;
+
 @Getter
 @Setter
 @ToString(callSuper = true)
-public class User extends BaseDomain{
+public class User {
+    //姓名
     private String name;
-
+    // 手机号
     private String phone;
-
+    // 图片
     private String picture;
-
+    // 性别
     private Integer sex;
-
-    public String getSexName(){
-        String sexName = null;
-        if(sex != null){
-            switch (sex){
-                case 1 : sexName = "男";break;
-                case 0 : sexName = "女";break;
-                default: sexName = "unkonw";
-            }
-        }
-        return sexName;
-    }
+    // id
+    private Integer id;
+    // 插入的操作人员
+    private String createBy;
+    // 插入的时间
+    private Date credateAt;
+    // 修改的操作人员
+    private String updateBy;
+    // 修改时间
+    private Date updateAt;
 }
--- druid-mybatis/src/test/java/com/hellen/springboot/mapper/UserMapperTest.java
+++ druid-mybatis/src/test/java/com/hellen/springboot/mapper/UserMapperTest.java
@@ -3,9 +3,11 @@ package com.hellen.springboot.mapper;
 import com.hellen.springboot.DruidMybatisApplicationTests;
 import com.hellen.springboot.domain.User;
 import lombok.extern.slf4j.Slf4j;
+import org.junit.Assert;
 import org.junit.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 
+import java.util.Date;
 import java.util.List;
 
 /**
@@ -25,8 +27,22 @@ public class UserMapperTest  extends DruidMybatisApplicationTests {
         for (User user : users) {
             log.info("姓名 ; [{}]",user.getName());
             log.info("电话 ; [{}]",user.getPhone());
-            log.info("性别 ; [{}]",user.getSexName());
             log.info("创建时间 ; [{}]",user.getCredateAt());
         }
+    }
+
+    @Test
+    public void testInsert(){
+        User user = new User();
+        user.setName("张三");
+        user.setPhone("18825155657");
+        user.setSex(1);
+        user.setPicture("/img");
+        user.setCreateBy("systemOperatr");
+        user.setCredateAt(new Date());
+        user.setUpdateBy("systemOperator");
+        user.setUpdateAt(new Date());
+        int insertNum = userMapper.insert(user);
+        Assert.assertEquals(1 , insertNum);
     }
 }
